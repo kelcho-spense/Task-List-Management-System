9c47c865e74b4e33249e248b376918d07640334e
kelcho-spense <kelchospense88@gmail.com>
2024-12-13 18:25:20 +0300

    added rate-limiting, cors, helmet & versioning

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { TasksModule } from './tasks/tasks.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -17,8 +19,20 @@ import { EmployeesModule } from './employees/employees.module';
     DatabaseModule,
     TasksModule,
     EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 1 minute of time to live
+        limit: 10, // 10 requests per TTL
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD, // Apply rate limiting to all routes
+      useClass: ThrottlerGuard, // Use ThrottlerGuard as the rate limiting guard
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // versioning
+  app.setGlobalPrefix('api/v1');
+  // setting HTTP headers appropriately
+  app.use(helmet());
+  //cors
+  app.enableCors();
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
